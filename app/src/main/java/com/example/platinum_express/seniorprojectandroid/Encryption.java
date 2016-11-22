bae22206261585489a9af6840ba6c4a108970d14
Marcel Henderson <marcelhenderson@Marcels-MacBook-Pro.local>
2016-11-21 23:56:10 -0500

    User Authentication

--- app/src/main/java/com/example/platinum_express/seniorprojectandroid/Encryption.java
+++ app/src/main/java/com/example/platinum_express/seniorprojectandroid/Encryption.java
@@ -17,13 +17,14 @@ import javax.crypto.spec.SecretKeySpec;
 
 public class Encryption
 {
-    public static String Encrypt(String text, String mySalt) throws Exception
+    public static boolean decrypt(String userPassword, String encryptedDBPassword) throws Exception
     {
         String password  = "TrackBGMKey12345";
         int iterationCount = 1000;
         int keyLength = 256;
         int saltLength = keyLength / 8; // same size as key output
         byte[] salt = new byte[saltLength];
+        String mySalt = getSalt(encryptedDBPassword);
         salt = mySalt.getBytes();
         String _vector = "8947az34awl34kjq";
 
@@ -31,14 +32,24 @@ public class Encryption
         SecretKeyFactory keyFactory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA1");
         byte[] keyBytes = keyFactory.generateSecret(keySpec).getEncoded();
         SecretKey myKey = new SecretKeySpec(keyBytes, "AES");
-
         Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
         byte[] iv = new byte[cipher.getBlockSize()];
+
         iv = _vector.getBytes();
         IvParameterSpec ivParams = new IvParameterSpec(iv);
         cipher.init(Cipher.ENCRYPT_MODE, myKey, ivParams);
-        byte[] ciphertext = cipher.doFinal(text.getBytes("UTF-8"));
+        byte[] ciphertext = cipher.doFinal(userPassword.getBytes("UTF-8"));
         String end = Base64.encodeToString(ciphertext,Base64.NO_WRAP);
-        return mySalt + end;
+        String encryptedUserPassword = mySalt + end;
+        return validated(encryptedUserPassword, encryptedDBPassword);
+    }
+
+    public static String getSalt(String encryptedPassword){
+        return encryptedPassword.substring(0, 32);
+    }
+
+
+    public static boolean validated(String encryptedUserPassword, String encryptedDBPassword){
+        return encryptedUserPassword.equals(encryptedDBPassword);
     }
 }
--- app/src/main/java/com/example/platinum_express/seniorprojectandroid/MainActivity.java
+++ app/src/main/java/com/example/platinum_express/seniorprojectandroid/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.platinum_express.seniorprojectandroid;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -10,10 +11,30 @@ import android.widget.EditText;
 import android.view.View.OnClickListener;
 import android.widget.TextView;
 
+import org.apache.http.NameValuePair;
+import org.apache.http.message.BasicNameValuePair;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity implements OnClickListener {
+
+    private static final String TAG_SUCCESS = "success";
+    private static final String JSON_DATA = "data";
+    private static final String JSON_PASSWORD = "User_Password_PBKDF2";
+    //private static String url_create_product = "http://10.0.2.2/SeniorProjectPHP/create_timesheet_record.php";
+    JSONParser jsonParser = new JSONParser();
+
     ClientDb db = new ClientDb(this);
     EditText username;
     EditText password;
+    TextView error;
     Button login_submit;
 
 
@@ -25,11 +46,34 @@ public class MainActivity extends AppCompatActivity implements OnClickListener {
         username = (EditText) findViewById(R.id.user_name);
         password = (EditText) findViewById(R.id.password);
         login_submit = (Button) findViewById(R.id.login_submit);
+        error = (TextView) findViewById(R.id.login_error);
         login_submit.setOnClickListener(this);
     }
 
     public void onClick(View view){
-        //ClientDb database = new ClientDb(view.getContext());
+        Log.d("login test", "in login");
+        Log.d("login test", "username=" + username.getText().toString());
+        Log.d("login test", "password=" + password.getText().toString());
+        try {
+            AuthenticateUser auth = new AuthenticateUser(username.getText().toString());
+            auth.execute().get();
+            Log.d("compare", "enter pass = " + password.getText().toString() + "encrypted= " + auth.encryptedPassword);
+            if(Encryption.decrypt(password.getText().toString(), auth.encryptedPassword)){
+                Intent myIntent = new Intent(this, Timesheet.class);
+                startActivity(myIntent);
+            }
+            else
+                error.setText("Invalid Username/Password");
+        } catch(Exception e) {
+            Log.d("login test", db.getPass(username.getText().toString()));
+            Log.d("Error", "Occurred during decryption");
+            error.setText("Invalid Username/Password");
+        }
+        if(username.getText().toString().equals("a") && password.getText().toString().equals("a"))
+        {
+            Intent myIntent = new Intent(this, Timesheet.class);
+            startActivity(myIntent);
+        }
         /*
         if (db.getPass(username.getText().toString()).equals("\0")){
             error.setText("Invalid Username/Password");
@@ -41,13 +85,51 @@ public class MainActivity extends AppCompatActivity implements OnClickListener {
         if(username.getText().toString().equals("admin") && password.getText().toString().equals("password") ||
                 (password.getText().toString().equals(db.getPass(username.getText().toString())))||
                 username.getText().toString().equals("a") && password.getText().toString().equals("a")){
-        */
-        if(username.getText().toString().equals("a") && password.getText().toString().equals("a")){
             Log.d("login test", "in login in if");
             Intent myIntent = new Intent(this, Timesheet.class);
             startActivity(myIntent);
         }else{
-           // error.setText("Invalid Username/Password");
+            error.setText("Invalid Username/Password");
+        }
+        */
+    }
+
+    class AuthenticateUser extends AsyncTask<String, String, String> {
+        String url_get_user_password = "http://10.0.2.2/SeniorProjectPHP/get_user_record.php";
+        JSONArray jsonResponse;
+        private String username;
+        private String encryptedPassword;
+
+        public AuthenticateUser(String username){
+            this.username = username;
+            this.encryptedPassword = "";
+            this.jsonResponse = null;
+            this.url_get_user_password += "?username='" + username + "'";
+        }
+        protected String doInBackground(String... args) {
+           List<NameValuePair> params = new ArrayList<NameValuePair>();
+            //params.add(new BasicNameValuePair("username", username));
+            Log.d("url", "url= " + url_get_user_password);
+            JSONObject json = jsonParser.makeHttpRequest(url_get_user_password,
+                    "GET", params);
+
+            Log.d("Getting User Password", json.toString());
+
+            try {
+                int success = json.getInt(TAG_SUCCESS);
+
+                if (success == 1) {
+                    jsonResponse = json.getJSONArray(JSON_DATA);
+                    JSONObject data = jsonResponse.getJSONObject(0);
+                    encryptedPassword = data.getString(JSON_PASSWORD);
+                    Log.d("Completed", "Insert Successful");
+                } else {
+                    Log.d("Error", "Insert Failed");
+                }
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+            return null;
         }
     }
 }
